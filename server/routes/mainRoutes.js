d7a2ec33b069b81622a9ebe66111cf0d4c80c40f
MarkVuyisile <116636579+MarkVuyisile@users.noreply.github.com>
2023-01-24 09:51:03 +0200

    Reviewing you code

--- server/routes/mainRoutes.js
+++ server/routes/mainRoutes.js
@@ -1,5 +1,9 @@
 const PhoneBook = require("../models/mySchemas");
 const savePhoneBook = (app) => {
+  /*
+  *Mark:
+  *I think the route name should be "/save/contact".
+  */
   app.post("/save", async (req, res) => {
     try {
       let { Firstname, Number, Email } = req.body;
@@ -18,6 +22,13 @@ const savePhoneBook = (app) => {
       res.send({ message: "Post Error" }).status(404);
     }
   });
+  
+  /*
+  *Mark:
+  *It's always good to keep the route names all lowercase.
+  *Bad naming, if you trying to get all contacts, then the route name should be "/contacts" or "/get/contacts".
+  *"findPhoneBooks" should be "allContacts" or "contacts"
+  */
   app.get("/Contact", async (req, res) => {
     try {
       const findPhoneBooks = await PhoneBook.find();
@@ -26,6 +37,12 @@ const savePhoneBook = (app) => {
       console.log({ message: "Get Error" });
     }
   });
+  
+  /*
+  *Mark:
+  *The route name should be "/contact/:id" or "/get/contact/:id".
+  *"findPhoneBook" should be "contact"
+  */
   app.get("/phone/:id", async (req, res) => {
     try {
       const findPhoneBook = await PhoneBook.findById(req.body.id);
@@ -34,7 +51,11 @@ const savePhoneBook = (app) => {
       console.log({ message: "Get Error" });
     }
   });
-
+  
+  /*
+  *Mark:
+  *I think it's better you name this route "/update/contact/:id"
+  */
 app.put('/update/:id', async (req,res) =>{
    const { id } = req.params;
    let { Firstname, Number, Email } = req.body;
@@ -48,11 +69,15 @@ app.put('/update/:id', async (req,res) =>{
       console.log({ message: "Editing Unsuccesfully" });
    }
 })
+  
+  /*
+  *Mark:
+  *Not sure whats supposed to happen on the function below...??
+  */
 app.put('/contact/:id',async(req,res)=>{
   try {
     const { id } = req.params;
     const FindContact = await PhoneBook.findByIdAndUpdate({id:id})
-    
     res.send({ message: "Succesfully Updated", FindContact});
  } catch (error) {
     console.log({ message: "Deleting Unsuccesfully" })
@@ -60,6 +85,10 @@ app.put('/contact/:id',async(req,res)=>{
  }
 })
 
+  /*
+  *Mark:
+  *I think it's better you name this route "/delete/contact/:id" and the succesful response shouble something like "Contact deleted"
+  */
   app.delete('/delete/:id', async (req, res) =>{
    try {
       const { id } = req.params;
